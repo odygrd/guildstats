27e33a0d8ac53ffc78cffa590353ca82bbe2b479
Odysseas Georgoudis <odygrd@gmail.com>
2021-09-12 13:01:44 +0100

    Initial Commit

--- flask_app.py
+++ flask_app.py
@@ -3,8 +3,18 @@
 
 from flask import Flask, render_template
 
+UPLOAD_FOLDER = '/uploads'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
     return render_template("main_page.html")
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
