c0b5cb5e63163ff74d877ab30301816f35d3f2c5
Odysseas Georgoudis <odygrd@gmail.com>
2021-09-12 13:58:34 +0100

    Initial Commit

--- flask_app.py
+++ flask_app.py
@@ -24,7 +24,7 @@ def get_latest_player_stats_file():
         filename = file.strip("'")
         filename = filename.split("_")[-1]
         date = filename.split(".")[0]
-        print("ODY Append date", date)
+        print("ODY Append date {} for file {} filename {}".format(date, file, filename))
         dates.append(date)
 
     # Sort the list in ascending order of dates
