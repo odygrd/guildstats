9a3c1f0e179db1e7cca72c31dddeecbe762b0a99
Odysseas Georgoudis <odygrd@gmail.com>
2021-09-12 13:08:07 +0100

    Initial Commit

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 UPLOAD_FOLDER = '/uploads'
 
@@ -12,9 +12,9 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
     return render_template("main_page.html")
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
