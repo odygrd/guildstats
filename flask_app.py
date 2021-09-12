15c43e02a5ec7ea1a6a237a0e8c818d606d9f5cb
Odysseas Georgoudis <odygrd@gmail.com>
2021-09-12 18:55:10 +0100

    Try using datatables.js

--- flask_app.py
+++ flask_app.py
@@ -16,6 +16,18 @@ UPLOAD_FOLDER = '/home/odygrd/guildstats/uploads'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+class PlayerInfo
+  def __init__(self, name, era, attack, defence, attdef, era_avg_attdef, goods, era_avg_goods):
+    self.name = name
+    self.era = era
+    self.attack = attack
+    self.defence = defence
+    self.attdef = attdef
+    self.era_avg_attdef = era_avg_attdef
+    self.goods = goods
+    self.era_avg_goods = era_avg_goods
+
+
 # Returns the file with the latest date
 def get_latest_player_stats_file():
     dates = []
@@ -52,19 +64,29 @@ def gen_player_stats_html():
     # Join into a single df
     df_final = df.join(df_mean)
     df_final.reset_index(drop=False, inplace=True)
-    column_names = ["Score", "Name", "Era", "Attack", "Defense", "Total (Att+Def)", "Era average Att+Def", "Guild Goods", "Era average Guild Goods"]
-    df_final = df_final.reindex(columns=column_names)
-    df_final.set_index('Score', inplace=True)
-    df_final.sort_index(ascending=False,inplace=True)
-    df_final.reset_index(drop=True, inplace=True)
 
-    styled_df = df_final.style.background_gradient(cmap=sns.light_palette("green", as_cmap=True), subset=pd.IndexSlice[df_final['Guild Goods']>=2357, 'Guild Goods']).background_gradient(cmap=sns.light_palette("red", as_cmap=True, reverse=True), subset=pd.IndexSlice[df_final['Guild Goods']<2357, 'Guild Goods']).background_gradient(cmap=sns.light_palette("purple", as_cmap=True), subset=['Total (Att+Def)']).set_precision(0).set_table_styles([{"selector": "", "props": [("border", "1px solid grey")]},{"selector": "tbody td", "props": [("border", "1px solid grey")]},{"selector": "th", "props": [("border", "1px solid grey")]}])
-    return styled_df.render()
+    players = []
+    for index, row in df.iterrows():
+        players_grid.append(PlayerInfo(name=row["Name"], era=row["Era"],
+                                       attack=row["Attack"], defence = row["Defense"], attdef= row["Total (Att+Def)"],
+                                       era_avg_attdef=row["Era average Att+Def"], goods=row["Guild Goods"],
+                                       era_avg_goods=row["Era average Guild Goods"]))
+    return players
+
+#     column_names = ["Score", "Name", "Era", "Attack", "Defense", "Total (Att+Def)", "Era average Att+Def", "Guild Goods", "Era average Guild Goods"]
+#     df_final = df_final.reindex(columns=column_names)
+#     df_final.set_index('Score', inplace=True)
+#     df_final.sort_index(ascending=False,inplace=True)
+#     df_final.reset_index(drop=True, inplace=True)
+#
+#     styled_df = df_final.style.background_gradient(cmap=sns.light_palette("green", as_cmap=True), subset=pd.IndexSlice[df_final['Guild Goods']>=2357, 'Guild Goods']).background_gradient(cmap=sns.light_palette("red", as_cmap=True, reverse=True), subset=pd.IndexSlice[df_final['Guild Goods']<2357, 'Guild Goods']).background_gradient(cmap=sns.light_palette("purple", as_cmap=True), subset=['Total (Att+Def)']).set_precision(0).set_table_styles([{"selector": "", "props": [("border", "1px solid grey")]},{"selector": "tbody td", "props": [("border", "1px solid grey")]},{"selector": "th", "props": [("border", "1px solid grey")]}])
+#     return styled_df.render()
 
 @app.route("/")
 def index():
-    data = gen_player_stats_html()
-    return render_template("index.html", data=data)
+    players = gen_player_stats_html()
+    return render_template('basic_table.html', title='Guild Stats',
+                           players=players)
 
 @app.route('/', methods=['POST'])
 def upload_file():
