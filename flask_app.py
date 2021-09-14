1fff0614c7ca1a030df0ab379fa890ea4a7c1081
Odysseas Georgoudis <odygrd@gmail.com>
2021-09-14 17:21:51 +0100

    minor

--- flask_app.py
+++ flask_app.py
@@ -95,8 +95,8 @@ def index():
     players, update_date, guild_average_guild_goods, total_guild_goods = gen_player_stats_grid()
     return render_template('basic_table.html', title='Guild Stats',
                            players=players, update_date=update_date,
-                           guild_average_guild_goods=guild_average_guild_goods,
-                           total_guild_goods=total_guild_goods)
+                           guild_average_guild_goods=f'{guild_average_guild_goods:,}',
+                           total_guild_goods=f'{total_guild_goods:,}')
 
 @app.route("/upload")
 def upload():
