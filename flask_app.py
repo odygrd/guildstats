69343c6ddf5c00086c55a2a1e7f2c388df77b948
Odysseas Georgoudis <odygrd@gmail.com>
2021-09-15 10:32:49 +0100

    add detail page

--- flask_app.py
+++ flask_app.py
@@ -98,6 +98,14 @@ def index():
                            guild_average_guild_goods=f'{guild_average_guild_goods:,}',
                            total_guild_goods=f'{total_guild_goods:,}')
 
+@app.route("/detail")
+def index():
+    players, update_date, guild_average_guild_goods, total_guild_goods = gen_player_stats_grid()
+    return render_template('detail.html',
+                           players=players, update_date=update_date,
+                           guild_average_guild_goods=f'{guild_average_guild_goods:,}',
+                           total_guild_goods=f'{total_guild_goods:,}')
+
 @app.route("/upload")
 def upload():
     return render_template('upload.html')
